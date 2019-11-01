7f47490bc87d93a9ae73de1a2c725a28226a5233
Greg Clarke <gregbclarke@gmail.com>
2019-11-01 12:43:55 +1100

    Add verbatimRank to marshallName(Name name) for the plant name project.

--- grails-app/services/au/org/biodiversity/nsl/JsonRendererService.groovy
+++ grails-app/services/au/org/biodiversity/nsl/JsonRendererService.groovy
@@ -272,7 +272,8 @@ class JsonRendererService {
                 exAuthor         : getBriefAuthor(name.exAuthor),
                 exBaseAuthor     : getBriefAuthor(name.exBaseAuthor),
                 sanctioningAuthor: getBriefAuthor(name.sanctioningAuthor),
-                primaryInstance  : instanceService.findPrimaryInstance(name)?.collect { Instance instance -> getBriefInstance(instance) }
+                primaryInstance  : instanceService.findPrimaryInstance(name)?.collect { Instance instance -> getBriefInstance(instance) },
+                verbatimName     : name.verbatimRank
         ]
 
         return data.name as Map
