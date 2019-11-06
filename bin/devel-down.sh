2750db1a24e8a04f79c731181f647c129da56f8a
pmcneil <peter@nerderg.com>
2019-11-07 09:11:12 +1100

    Doco and mod to devel-down.sh to stop docker-compose complaining.

--- bin/devel-down.sh
+++ bin/devel-down.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
 
+#just exporting these environment variables to stop docker-compose complaining
+HOST_ADDR="$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')"
+export HOST_ADDR
+export BASE_DOMAIN=biodiversity.local
+export DOMAIN_PREFIX=""
+export DOMAIN_PREFIX_DASH=""
+export DB_HOST="postgresql://${HOST_ADDR}:5432"
+
+VOLUME_ROOT="$(pwd)"
+export VOLUME_ROOT
+
 docker-compose -f etc/devel-docker-compose.yml down
