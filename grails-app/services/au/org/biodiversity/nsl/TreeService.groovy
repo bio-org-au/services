e1abd63e9f0b61261b2a00bda794f0f82fc4e4ef
pmcneil <peter@nerderg.com>
2019-11-14 16:43:07 +1100

    Attempt to fix cases where we display two tree profiles because two drafts were created by the later draft was committed first. So now we rely on published at to order findLatestElementForInstance()

--- grails-app/services/au/org/biodiversity/nsl/TreeService.groovy
+++ grails-app/services/au/org/biodiversity/nsl/TreeService.groovy
@@ -160,7 +160,7 @@ class TreeService implements ValidationUtils {
     TreeVersionElement findLatestElementForInstance(Instance instance, Tree tree) {
         if (instance && tree) {
             return TreeVersionElement.find(
-                    'from TreeVersionElement where treeVersion.tree = :tree and treeElement.instanceId = :instanceId and treeVersion.published = true order by treeVersion.id desc',
+                    'from TreeVersionElement where treeVersion.tree = :tree and treeElement.instanceId = :instanceId and treeVersion.published = true order by treeVersion.publishedAt desc',
                     [tree: tree, instanceId: instance.id])
         }
         return null
