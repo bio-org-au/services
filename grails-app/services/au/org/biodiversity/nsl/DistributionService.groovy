3d27d32dac3a4525849426148da9c91f287358f8
Chris <chris.bitmead@gmail.com>
2021-10-06 10:02:02 +1100

    NSL-4003 rollback optimisation

--- grails-app/services/au/org/biodiversity/nsl/DistributionService.groovy
+++ grails-app/services/au/org/biodiversity/nsl/DistributionService.groovy
@@ -40,37 +40,29 @@ class DistributionService {
     }
 
     TreeElement removeDistributionEntries(TreeElement element) {
-        boolean modified = false
         if (element.distributionEntries) {
             List<DistEntry> entries = new LinkedList<DistEntry>(element.distributionEntries)
             entries.each { DistEntry entry ->
                 element.removeFromDistributionEntries(entry)
-                modified = true
+                // From the documentation: "isDirty() does not currently check collection associations, but it does check all other persistent properties and associations."
+                // I have no idea why the save(flush) is needed to force a save
+                element.save(flush: true)
+                element.markDirty()
             }
-        }
-        if (modified) {
-            // From the documentation: "isDirty() does not currently check collection associations, but it does check all other persistent properties and associations."
-            // I have no idea why the save(flush) is needed to force a save
-            element.save(flush: true)
-            element.markDirty()
         }
         return element
     }
 
     void reconstructDistribution(TreeElement element, String dist, Boolean ignoreErrors = false) {
         removeDistributionEntries(element)
-        boolean modified = false
         if (dist) {
             deconstructDistributionString(dist, ignoreErrors).each { DistEntry entry ->
                 element.addToDistributionEntries(entry)
-                modified = true
+                // From the documentation: "isDirty() does not currently check collection associations, but it does check all other persistent properties and associations."
+                // I have no idea why the save(flush) is needed to force a save
+                element.save(flush: true)
+                element.markDirty()
             }
-        }
-        if (modified) {
-            // From the documentation: "isDirty() does not currently check collection associations, but it does check all other persistent properties and associations."
-            // I have no idea why the save(flush) is needed to force a save
-            element.save(flush: true)
-            element.markDirty()
         }
     }
 }
