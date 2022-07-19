a150cc628eaa0660d258b209a15c028cef818d1f
Mo Ziauddin <moziauddin@gmail.com>
2022-07-19 15:59:19 +1000

    Reverted config changes and add AD and LDAP changes

--- etc/nsl/services-config-g3.groovy
+++ etc/nsl/services-config-g3.groovy
@@ -1,5 +1,4 @@
 
-grails.serverURL = "http://biodiversity.local:8080/nsl/services"
 
 shard {
     system.message.file = "/etc/nsl/broadcast.txt"
@@ -37,26 +36,24 @@ security {
     shiro {
         realm {
             ldap {
-                server.urls = 'ldap://biodiversity.local:10389'
-                userName.attribute = 'uid'
+                server.urls = 'ldap://172.31.128.116:389'
+                username.attribute = 'sAMAccountName'
 
                 search {
-                    base = 'ou=users,o=shards'
-                    user = 'uid=admin,ou=system'
-                    pass = 'secret'
+                    base = 'dc=cloud,dc=biodiversity,dc=org,dc=au'
+//                    base = 'ou=users,ou=nsl,dc=cloud,dc=biodiversity,dc=org,dc=au'
+//                    base = 'cn=Users,dc=cloud,dc=biodiversity,dc=org,dc=au'
+                    user = 'cn=NSL Admin,ou=users,ou=nsl,dc=cloud,dc=biodiversity,dc=org,dc=au'
+                    pass = 'askgreg'
 
                     group {
-                        name = "ou=groups,ou=vasc,o=shards"
+                        name = 'dc=cloud,dc=biodiversity,dc=org,dc=au'
                         member {
-                            element = 'uniqueMember'
-                            prefix = 'uid='
-                        }
-                    }
-                    permission {
-                        commonName = 'cn=permission'
-                        member {
-                            element = 'uniqueMember'
-                            prefix = 'uid='
+                            element = 'sAMAccountName'
+                            memberAttribute = 'memberof'
+                            groupPattern = 'CN=([^,]*),OU=apni,OU=test,OU=nsl'
+                            prefix = ''
+                            postfix = ''
                         }
                     }
                 }
@@ -64,6 +61,38 @@ security {
         }
     }
 }
+
+//security {
+//    shiro {
+//        realm {
+//            ldap {
+//                server.urls = 'ldap://appstst1-ibis.cloud.biodiversity.org.au:10389'
+//                userName.attribute = 'uid'
+//
+//                search {
+//                    base = 'ou=users,dc=nsl,dc=bio,dc=org,dc=au'
+//                    user = 'uid=admin,ou=system'
+//                    pass = 'secret'
+//
+//                    group {
+//                        name = 'ou=groups,dc=apni,dc=nsl,dc=bio,dc=org,dc=au'
+//                        member {
+//                            element = 'uniqueMember'
+//                            prefix = 'uid='
+//                        }
+//                    }
+//                    permission {
+//                        commonName = 'cn=permission'
+//                        member {
+//                            element = 'uniqueMember'
+//                            prefix = 'uid='
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
 
 dataSource {
     username = 'nsl'
