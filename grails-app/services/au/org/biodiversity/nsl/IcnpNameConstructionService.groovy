75c314dcabf1ad191925c26e673e2ff0cffdf94d
Mo Ziauddin <moziauddin@gmail.com>
2022-12-12 21:41:51 +1100

    Change ICNP name construction services to use CodecLookup

--- grails-app/services/au/org/biodiversity/nsl/IcnpNameConstructionService.groovy
+++ grails-app/services/au/org/biodiversity/nsl/IcnpNameConstructionService.groovy
@@ -16,21 +16,25 @@
 
 package au.org.biodiversity.nsl
 
+import groovy.transform.CompileStatic
+import org.grails.encoder.CodecLookup
+
 class IcnpNameConstructionService implements NameConstructor {
 
+    CodecLookup codecLookup
     static transactional = false
 
     static String join(List<String> bits) {
         bits.findAll { it }.join(' ')
     }
 
+    @CompileStatic
     ConstructedName constructName(Name name) {
         if (!name) {
             throw new NullPointerException("Name can't be null.")
         }
 
         if (name.nameType.scientific) {
-
             if (name.nameType.autonym) {
                 return constructAutonymScientificName(name)
             }
@@ -42,66 +46,83 @@ class IcnpNameConstructionService implements NameConstructor {
         }
 
         if (name.nameType.nameCategory?.name == 'common') {
-            String htmlNameElement = encodeHtml(name.nameElement)
+            String htmlNameElement = htmlEncoder.encode(name.nameElement)
             String markedUpName = "<common><name data-id='$name.id'><element>${htmlNameElement}</element></name></common>"
-            return [fullMarkedUpName: markedUpName, simpleMarkedUpName: markedUpName]
+            return new ConstructedName(fullMarkedUpName: markedUpName, simpleMarkedUpName: markedUpName)
         }
-
-        return [fullMarkedUpName: (encodeHtml(name.nameElement) ?: '?'), simpleMarkedUpName: (encodeHtml(name.nameElement) ?: '?')]
+        String defaultName = (name.nameElement ? htmlEncoder.encode(name.nameElement) : '?')
+        return new ConstructedName(fullMarkedUpName: defaultName, simpleMarkedUpName: defaultName)
     }
 
-    private static ConstructedName constructInformalName(Name name) {
-        List<String> bits = ["<element>${encodeHtml(name.nameElement)}</element>", constructAuthor(name)]
+    @CompileStatic
+    private ConstructedName constructInformalName(Name name) {
+        List<String> bits = ["<element>${htmlEncoder.encode(name.nameElement)}</element>".toString(), constructAuthor(name)]
+        String joined = join(bits)
 
-        String markedUpName = "<informal><name data-id='$name.id'>${join(bits)}</name></informal>"
+        String markedUpName = "<informal><name data-id='$name.id'>${joined}</name></informal>"
         return new ConstructedName(fullMarkedUpName: markedUpName, simpleMarkedUpName: markedUpName)
     }
 
+    @CompileStatic
+    private ConstructedName constructAutonymScientificName(Name name) {
+        Name nameParent = NameConstructionUtils.nameParent(name)
+        String precedingName = constructPrecedingNameString(nameParent, name)
+        String rank = nameParent ? makeRankString(name) : ''
+        String connector = makeConnectorString(name, rank)
+        String element = "<element>${htmlEncoder.encode(name.nameElement)}</element>"
+        String manuscript = (name.nameStatus.name == 'manuscript') ? '<manuscript>MS</manuscript>' : ''
+
+        List<String> simpleNameParts = [precedingName, rank, connector, element, manuscript]
+
+        String fullMarkedUpName = "<scientific><name data-id='$name.id'>${join(simpleNameParts)}</name></scientific>"
+        //need to remove Authors below from simple name because preceding name includes author in autonyms
+        return new ConstructedName(fullMarkedUpName: fullMarkedUpName, simpleMarkedUpName: NameConstructionUtils.removeAuthors(fullMarkedUpName))
+    }
+
+    @CompileStatic
     private ConstructedName constructScientificName(Name name) {
-        use(NameConstructionUtils) {
-            Name nameParent = name.nameParent()
-            String precedingName = constructPrecedingNameString(nameParent, name)
+        Name nameParent = NameConstructionUtils.nameParent(name)
+        String precedingName = constructPrecedingNameString(nameParent, name)
 
-            if (nameParent && !precedingName) {
-                log.error "parent $nameParent, but didn't construct name."
-            }
-
-            String candOpen = name.nameType.name == 'candidatus' ? '<candidatus>Candidatus' : ''
-            String candClose = name.nameType.name == 'candidatus' ? '</candidatus>' : ''
-            String rank = nameParent ? makeRankString(name) : ''
-            String connector = makeConnectorString(name, rank)
-            String element = "<element>${encodeHtml(name.nameElement)}</element>"
-            String author = constructAuthor(name)
-            String manuscript = (name.nameStatus.name == 'manuscript') ? '<manuscript>MS</manuscript>' : ''
-
-            List<String> fullNameParts = [candOpen, precedingName, rank, connector, element, candClose, author, manuscript]
-            List<String> simpleNameParts = [candOpen, precedingName, rank, connector, element, candClose, manuscript]
-
-            String fullMarkedUpName = "<scientific><name data-id='$name.id'>${join(fullNameParts)}</name></scientific>"
-            String simpleMarkedUpName = "<scientific><name data-id='$name.id'>${join(simpleNameParts)}</name></scientific>"
-            return new ConstructedName(fullMarkedUpName: fullMarkedUpName, simpleMarkedUpName: simpleMarkedUpName)
+        if (nameParent && !precedingName) {
+            log.error "parent $nameParent, but didn't construct name."
         }
+
+        String candOpen = name.nameType.name == 'candidatus' ? '<candidatus>Candidatus' : ''
+        String candClose = name.nameType.name == 'candidatus' ? '</candidatus>' : ''
+        String rank = nameParent ? makeRankString(name) : ''
+        String connector = makeConnectorString(name, rank)
+        String element = "<element>${htmlEncoder.encode(name.nameElement)}</element>"
+        String author = constructAuthor(name)
+        String manuscript = (name.nameStatus.name == 'manuscript') ? '<manuscript>MS</manuscript>' : ''
+
+        List<String> fullNameParts = [candOpen, precedingName, rank, connector, element, candClose, author, manuscript]
+        List<String> simpleNameParts = [candOpen, precedingName, rank, connector, element, candClose, manuscript]
+
+        String fullMarkedUpName = "<scientific><name data-id='$name.id'>${join(fullNameParts)}</name></scientific>"
+        String simpleMarkedUpName = "<scientific><name data-id='$name.id'>${join(simpleNameParts)}</name></scientific>"
+        return new ConstructedName(fullMarkedUpName: fullMarkedUpName, simpleMarkedUpName: simpleMarkedUpName)
     }
 
+    @CompileStatic
     private String constructPrecedingNameString(Name parent, Name child) {
-        use(NameConstructionUtils) {
-            if (parent) {
-                Map constructedName = constructName(parent)
-                if (child.nameType.autonym) {
-                    return constructedName.fullMarkedUpName.removeManuscript()
-                }
-                if (child.nameType.formula) {
-                    if (parent.nameType.formula) {
-                        return "(${constructedName.fullMarkedUpName.removeManuscript()})"
-                    }
-                    return constructedName.fullMarkedUpName.removeManuscript()
-                }
-                return constructedName.simpleMarkedUpName.removeManuscript().replaceAll(/<candidatus>Candidatus ?(.*)<\/candidatus>/,'$1')
+        if (parent) {
+            ConstructedName constructedName = constructName(parent)
+            if (child.nameType.autonym) {
+                return constructedName.fullSansMS
             }
-            return ''
+            if (child.nameType.formula) {
+                if (parent.nameType.formula) {
+                    return "(${constructedName.fullSansMS})"
+                }
+                return constructedName.fullSansMS
+            }
+            return constructedName.simpleSansMS.replaceAll(/<candidatus>Candidatus ?(.*)<\/candidatus>/,'$1')
         }
+        return ''
     }
 
+    @CompileStatic
     private static String makeConnectorString(Name name, String rank) {
         if (name.nameType.connector &&
                 !(rank && name.nameType.connector == 'x' && name.nameRank.abbrev.startsWith('notho'))) {
@@ -111,6 +132,7 @@ class IcnpNameConstructionService implements NameConstructor {
         }
     }
 
+    @CompileStatic
     private static String makeRankString(Name name) {
         if (name.nameRank?.visibleInName) {
             if (name.nameRank.useVerbatimRank && name.verbatimRank) {
@@ -121,22 +143,23 @@ class IcnpNameConstructionService implements NameConstructor {
         return ''
     }
 
+    @CompileStatic
     String constructAuthor(Name name) {
         List<String> bits = []
         if (name.author) {
             if (name.baseAuthor) {
                 if (name.exBaseAuthor) {
-                    bits << "(<ex-base data-id='$name.exBaseAuthor.id' title='${encodeHtml(name.exBaseAuthor.name)}'>$name.exBaseAuthor.abbrev</ex-base> ex <base data-id='$name.baseAuthor.id' title='${encodeHtml(name.baseAuthor.name)}'>$name.baseAuthor.abbrev</base>)"
+                    bits << "(<ex-base data-id='$name.exBaseAuthor.id' title='${encodeHtml(name.exBaseAuthor.name)}'>$name.exBaseAuthor.abbrev</ex-base> ex <base data-id='$name.baseAuthor.id' title='${htmlEncoder.encode(name.baseAuthor.name)}'>$name.baseAuthor.abbrev</base>)".toString()
                 } else {
-                    bits << "(<base data-id='$name.baseAuthor.id' title='${encodeHtml(name.baseAuthor.name)}'>$name.baseAuthor.abbrev</base>)"
+                    bits << "(<base data-id='$name.baseAuthor.id' title='${encodeHtml(name.baseAuthor.name)}'>$name.baseAuthor.abbrev</base>)".toString()
                 }
             }
             if (name.exAuthor) {
-                bits << "(ex <ex data-id='$name.exAuthor.id' title='${encodeHtml(name.exAuthor.name)}'>$name.exAuthor.abbrev</ex>)"
+                bits << "<ex data-id='$name.exAuthor.id' title='${encodeHtml(name.exAuthor.name)}'>$name.exAuthor.abbrev</ex> ex".toString()
             }
-            bits << "<author data-id='$name.author.id' title='${encodeHtml(name.author.name)}'>$name.author.abbrev</author>"
+            bits << "<author data-id='$name.author.id' title='${encodeHtml(name.author.name)}'>$name.author.abbrev</author>".toString()
             if (name.sanctioningAuthor) {
-                bits << ": <sanctioning data-id='$name.sanctioningAuthor.id' title='${encodeHtml(name.sanctioningAuthor.name)}'>$name.sanctioningAuthor.abbrev</sanctioning>"
+                bits << ": <sanctioning data-id='$name.sanctioningAuthor.id' title='${encodeHtml(name.sanctioningAuthor.name)}'>$name.sanctioningAuthor.abbrev</sanctioning>".toString()
             }
         }
         return bits.size() ? "<authors>${join(bits)}</authors>" : ''
