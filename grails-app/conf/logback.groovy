a4c432e7040766418c1a08020a12b46a732590ba
Chris Bitmead <chris.bitmead@gmail.com>
2022-12-19 14:16:15 +0800

    change logging parameters

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -38,7 +38,7 @@ appender("dailyFileAppender", RollingFileAppender) {
     }
 }
 
-println "ENV: ${Environment.getCurrent()} DEV: ${Environment.isDevelopmentMode()}"
+println "logback environment: ${Environment.getCurrent()} isDevelopment: ${Environment.isDevelopmentMode()}"
 if (Environment.isDevelopmentMode()) {
     root(WARN, ['STDOUT'])
     logger("au.org.biodiversity", DEBUG, ['STDOUT'], false)
@@ -47,8 +47,12 @@ if (Environment.isDevelopmentMode()) {
     root(ERROR, ['STDOUT'])
     logger("au.org.biodiversity", DEBUG, ['STDOUT'], false)
     logger("services3", DEBUG, ['STDOUT'], false)
+} else if (Environment.getCurrent() == Environment.TEST)  {
+    root(DEBUG, ['STDOUT'])
+    logger("au.org.biodiversity", DEBUG, ['STDOUT'], false)
+    logger("services3", DEBUG, ['STDOUT'], false)
 } else {
-    root(ERROR, ['STDOUT'])
+    root(DEBUG, ['STDOUT'])
     logger("au.org.biodiversity", DEBUG, ['STDOUT'], false)
     logger("services3", DEBUG, ['STDOUT'], false)
 }
