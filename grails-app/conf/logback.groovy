ce35aef6a55ad0723bce65c410dceacd30201c27
Chris Bitmead <chris.bitmead@gmail.com>
2023-04-26 13:09:43 +0800

    NSL-4494 - add debug to routine

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -20,27 +20,14 @@ appender('STDOUT', ConsoleAppender) {
                         '%clr(%-15.20logger{39}){cyan} %clr(:){faint} ' + // Logger
                         '%msg %ex{1}%nopex%n' // Message
     }
-}
-
-appender("dailyFileAppender", RollingFileAppender) {
-    encoder(PatternLayoutEncoder) {
-        charset = Charset.forName('UTF-8')
-
-        pattern =
-                '%clr(%d{yyyy-MM-dd HH:mm:ss}){magenta} ' + // Date
-                        '%clr(%5p) ' + // Log level
-                        '%clr([%4.5t]){green} ' + // Thread
-                        '%clr(%-15.20logger{39}){cyan} %clr(:){faint} ' + // Logger
-                        '%msg %ex{1}%nopex%n' // Message
-    }
-    rollingPolicy(TimeBasedRollingPolicy) {
-        FileNamePattern = "${(System.getProperty('catalina.base') ?: (System.getProperty('user.home') ?: 'target'))}/logs/services-%d{yyyy-MM-dd}.zip"
-    }
+    logger("StackTrace", ERROR, ['STDOUT'], false)
+    logger("org.hibernate.orm.deprecation", OFF)
+    root(WARN, ['STDOUT'])
 }
 
 println "logback environment: ${Environment.getCurrent()} isDevelopment: ${Environment.isDevelopmentMode()}"
 if (Environment.isDevelopmentMode()) {
-    root(WARN, ['STDOUT'])
+    root(DEBUG, ['STDOUT'])
     logger("au.org.biodiversity", DEBUG, ['STDOUT'], false)
     logger("services3", DEBUG, ['STDOUT'], false)
 } else if (Environment.getCurrent() == Environment.PRODUCTION) {
@@ -48,7 +35,7 @@ if (Environment.isDevelopmentMode()) {
     logger("au.org.biodiversity", DEBUG, ['STDOUT'], false)
     logger("services3", DEBUG, ['STDOUT'], false)
 } else if (Environment.getCurrent() == Environment.TEST)  {
-    root(WARN, ['STDOUT'])
+    root(DEBUG, ['STDOUT'])
     logger("au.org.biodiversity", DEBUG, ['STDOUT'], false)
     logger("services3", DEBUG, ['STDOUT'], false)
 } else {
